6a9f2e6982f6a2d2782b54790275172eb9b0bf39
yeohj07 <yeohj0710@yonsei.ac.kr>
2023-01-15 22:56:27 +0900

    2301152256

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-export const formatHashtags = (hashtags) =>
-    hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`))
-
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true, maxLength: 80 },
     description: { type: String, required: true, trim: true},
@@ -13,6 +10,10 @@ const videoSchema = new mongoose.Schema({
         rating: { type: Number, default: 0, required: true },
     },
 });
+
+videoSchema.static('formatHashtags', function(hashtags) {
+    return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`))
+})
 
 const Video = mongoose.model("Video", videoSchema);
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { 
     watch, 
-    upload,
-    deleteVideo,
+    getUpload,
     getEdit, 
     postEdit,
-    getUpload,
     postUpload,
+    deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;

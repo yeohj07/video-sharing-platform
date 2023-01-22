db558cf6cbf7506e4a7fe7d0fc70141762d297fb
yeohj07 <yeohj0710@yonsei.ac.kr>
2023-01-22 20:01:55 +0900

    s3 uploads

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -71,8 +71,9 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].path,
-      thumbUrl: `/${thumb[0].destination}/${thumb[0].filename}`,
+      fileUrl: video[0].location,
+      thumbUrl: thumb[0].location,
+      // thumbUrl: `/${thumb[0].destination}/${thumb[0].filename}`,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
